fafe25e99a32fffad3261cd8eb98c5bd5fd30805
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-12-29 02:06:40 +0900

    removed unnecessary imports

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -5,17 +5,9 @@ import { OAuthModule } from 'angular-oauth2-oidc';
 import { RoutingModule } from './routing.module';
 import { SharedModule } from './shared/shared.module';
 
-import { CatService } from './services/cat.service';
 import { UserService } from './services/user.service';
 import { AuthService } from './services/auth.service';
 import { AppComponent } from './app.component';
-import { CatsComponent } from './cats/cats.component';
-import { AboutComponent } from './about/about.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { LogoutComponent } from './logout/logout.component';
-import { AccountComponent } from './account/account.component';
-import { AdminComponent } from './admin/admin.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ResearchMainComponent } from './research-main/research-main.component';
 import { ResearchLoginComponent } from './research-login/research-login.component';
@@ -41,7 +33,7 @@ import { OmniTrackPackageEditComponent } from './research/omnitrack/omnitrack-pa
   declarations: [
     AppComponent,
     //CatsComponent,
-    AboutComponent,
+    //AboutComponent,
     //RegisterComponent,
     //LoginComponent,
     //LogoutComponent,
--- client/app/routing.module.ts
+++ client/app/routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { CatsComponent } from './cats/cats.component';
 import { AboutComponent } from './about/about.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { LogoutComponent } from './logout/logout.component';
-import { AccountComponent } from './account/account.component';
-import { AdminComponent } from './admin/admin.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 import { AuthGuardAdmin } from './services/auth-guard-admin.service';
