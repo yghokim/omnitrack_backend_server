175b6fcc1ecd107b5a55dd543e493c05cb8376fb
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-12-30 23:11:22 +0900

    added busy-overlay

--- client/app/busy-overlay/busy-overlay.component.ts
+++ client/app/busy-overlay/busy-overlay.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'busy-overlay',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusyOverlayComponent implements OnInit {
 
+  @Input()
+  scale = 1.0
+
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
