a1b7a81651247198fcb41d924d64a7ac775ce568
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2018-01-11 10:45:51 +0900

    implementing raw data view

--- client/app/experiment-data/experiment-data.component.ts
+++ client/app/experiment-data/experiment-data.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
+import { ResearchApiService } from '../services/research-api.service';
+import { NotificationService } from '../services/notification.service';
 
 @Component({
   selector: 'app-experiment-data',
@@ -10,16 +12,43 @@ export class ExperimentDataComponent implements OnInit, OnDestroy {
 
   private _internalSubscriptions = new Subscription()
 
-  constructor() { 
+  private participants: Array<any>
+
+  private selectedParticipantId: string
+  
+  constructor(
+    private api: ResearchApiService,
+    private notificationService: NotificationService
+  ) { 
 
   }
 
   ngOnInit() {
-
+    this.notificationService.registerGlobalBusyTag("participantsInDataComponent")
+    this._internalSubscriptions.add(
+      this.api.selectedExperimentService.flatMap(service => service.getParticipants())
+        .subscribe(participants => {
+          this.participants = participants
+          if(this.participants.length > 0)
+          {
+            this.selectedParticipantId = this.participants[0]._id          
+            this.onSelectedParticipantIdChanged(this.selectedParticipantId)
+          }
+          this.notificationService.unregisterGlobalBusyTag("participantsInDataComponent")
+        })
+    )
   }
 
   ngOnDestroy(): void {
     this._internalSubscriptions.unsubscribe()
+  }
+  
+  onParticipantSelectionChanged(event){
+    console.log(event)
+  }
+
+  private onSelectedParticipantIdChanged(newParticipantId: string){
+    
   }
 
 }
