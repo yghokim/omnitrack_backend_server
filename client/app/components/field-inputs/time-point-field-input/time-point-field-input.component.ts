97d66d687d442ed10a19094b459241c0af8ffb50
valerieerb <valerie.erb@uzh.ch>
2018-04-11 15:03:28 +0900

    Time Input Field

--- client/app/components/field-inputs/time-point-field-input/time-point-field-input.component.ts
+++ client/app/components/field-inputs/time-point-field-input/time-point-field-input.component.ts
@@ -3,6 +3,7 @@ import { BaseItemFieldInputComponent } from '../base-item-field-input.component'
 import { TimePoint } from '../../../../../omnitrack/core/datatypes/field_datatypes';
 import * as moment from 'moment-timezone';
 import TypedStringSerializer from '../../../../../omnitrack/core/typed_string_serializer';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-time-point-field-input',
@@ -11,12 +12,16 @@ import TypedStringSerializer from '../../../../../omnitrack/core/typed_string_se
 })
 export class TimePointFieldInputComponent extends BaseItemFieldInputComponent implements OnInit {
 
-  time: Date
-
+  date: Date
+  hours: Array<number> = [];
+  minutes: Array<number> = [];
+ 
   protected onNewValue(attributeType: number, serializedValue?: string, deserializedValue?: any) {
     if(deserializedValue){
-    this.time = (deserializedValue as TimePoint).toDate()
+    this.date = (deserializedValue as TimePoint).toDate()
     }
+    for (let i: number = 0; i < 25; i++) { this.hours[i] = i; }
+    for (let i: number = 0; i < 60; i++) { this.minutes[i] = i; }
   }
 
   constructor() {
@@ -26,12 +31,28 @@ export class TimePointFieldInputComponent extends BaseItemFieldInputComponent im
   ngOnInit() {
   }
 
-  onTimeChanged(event){
-    const value = moment(event)
+  onDateChanged(event){
+    this.date = event.value;
+    const value = moment(event.value)
+    this.updateDateTime(value);
+  }
+
+  onTimeChanged(time: number, event){
+    if(time === 1){
+      this.date.setHours(event.value);
+    }
+    else{
+      this.date.setMinutes(event.value);
+    }
+    this.updateDateTime(moment(this.date));
+  }
+
+  updateDateTime(value: moment.Moment){
     if(value.isValid() === true){
       (this.deserializedValue as TimePoint).timestamp = value.toDate().getTime()
       this.setNewDeserializedValue(this.deserializedValue)
     }
   }
+
 
 }
