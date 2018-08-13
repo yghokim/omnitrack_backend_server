267911aa2e7e3484ee5b59de22b5891845f0acfa
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2018-08-14 01:31:43 +0900

    added a shortUrl copy button on client list.

--- client/app/components/client-binary-list/client-binary-list.component.ts
+++ client/app/components/client-binary-list/client-binary-list.component.ts
@@ -1,9 +1,12 @@
 import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { ResearchApiService } from '../../services/research-api.service';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 import { MatDialog } from '@angular/material/dialog';
 import { YesNoDialogComponent } from '../../dialogs/yes-no-dialog/yes-no-dialog.component';
 import { filter, flatMap } from 'rxjs/operators';
+import { Http } from '@angular/http';
+import { NotificationService } from '../../services/notification.service';
 
 @Component({
   selector: 'app-client-binary-list',
@@ -14,17 +17,53 @@ export class ClientBinaryListComponent implements OnInit, OnDestroy {
 
   private readonly _internalSubscriptions = new Subscription()
 
+  @Input() hideChangelogs: boolean = false
   @Input() experimentId: string
   @Input() useConfirmColumn: boolean
   @Input() binaries: Array<any>
 
-  constructor(private api: ResearchApiService, private dialog: MatDialog) { }
+  constructor(private api: ResearchApiService, private notificationService: NotificationService, private dialog: MatDialog, private http: Http) { }
 
   ngOnInit() {
   }
 
   ngOnDestroy(): void {
     this._internalSubscriptions.unsubscribe()
+  }
+
+  makeBinaryDownloadUrl(binary: any): string {
+    return '/api/clients/download?platform=' + binary.platform + '&version=' + binary.version + (this.experimentId ? ('&experimentId=' + this.experimentId) : '')
+  }
+
+  retrieveShortUrl(binary: any): Observable<string> {
+    const url = window.location.protocol + "//" + window.location.host + this.makeBinaryDownloadUrl(binary)
+    return this.http.get('http://tinyurl.com/api-create.php?url=' + encodeURIComponent(url)).pipe(
+      map(res => res.text()),
+      catchError(err => {
+        console.error(err)
+        this.notificationService.pushSnackBarMessage({ message: "An error occurred retrieving a short url." })
+        throw err
+      })
+    )
+  }
+
+  onShortUrlCopied(binary: any){
+    this.notificationService.pushSnackBarMessage({ message: "A shorted URL was copied to clipboard." })
+  }
+
+  onRetrieveLinkClicked(binary: any) {
+    const url = window.location.protocol + "//" + window.location.host + this.makeBinaryDownloadUrl(binary)
+    this._internalSubscriptions.add(
+      this.http.get('http://tinyurl.com/api-create.php?url=' + encodeURIComponent(url)).subscribe(shorten => {
+        console.log(shorten)
+      },
+        err => {
+          console.error(err)
+          this.notificationService.pushSnackBarMessage({ message: "An error occurred retrieving a short url." })
+        }
+      )
+    )
+
   }
 
   onRemoveBinaryClicked(binaryId: string) {
@@ -60,7 +99,7 @@ export class ClientBinaryListComponent implements OnInit, OnDestroy {
 
   hasBinariesToPublish(): boolean {
     if (this.binaries) {
-      if(this.binaries.find(b => b.needsConfirm === true)){
+      if (this.binaries.find(b => b.needsConfirm === true)) {
         return true
       }
     } else return false
