849bc3931ecaa81bda345fef4ee41676ff361de9
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2019-07-10 08:22:25 +0900

    implemented time field input previews

--- client/app/research/omnitrack/field-inputs/time/time-field-mobile-input/time-field-mobile-input.component.ts
+++ client/app/research/omnitrack/field-inputs/time/time-field-mobile-input/time-field-mobile-input.component.ts
@@ -1,19 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
 import { FieldMobileInputComponentBase } from '../../field-mobile-input-base.component';
 import { TimePointFieldHelper } from '../../../../../../../omnitrack/core/fields/time-point.field.helper';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-time-field-mobile-input',
   templateUrl: './time-field-mobile-input.component.html',
   styleUrls: ['./time-field-mobile-input.component.scss']
 })
-export class TimeFieldMobileInputComponent extends FieldMobileInputComponentBase<TimePointFieldHelper> implements OnInit {
+export class TimeFieldMobileInputComponent extends FieldMobileInputComponentBase<TimePointFieldHelper> implements OnInit, AfterContentChecked {
+
+  current = moment(Date.now())
+
+  showDateButton = false
+  dialValues: [string, string, string] = ["0", "0", "0"]
 
   constructor() {
     super()
-   }
+  }
 
   ngOnInit() {
   }
 
+  ngAfterContentChecked() {
+    switch (this.getFieldHelper().getParsedPropertyValue(this.field, TimePointFieldHelper.PROPERTY_GRANULARITY)) {
+      case TimePointFieldHelper.GRANULARITY_DAY:
+        this.showDateButton = false
+        this.dialValues = [
+          this.current.format('YYYY'),
+          this.current.format('MMM'),
+          this.current.format('DD').toUpperCase(),
+        ]
+        break;
+
+      case TimePointFieldHelper.GRANULARITY_MINUTE:
+        this.showDateButton = true
+        this.dialValues = [
+          this.current.format('hh'),
+          this.current.format('mm'),
+          this.current.format('A').toUpperCase(),
+        ]
+        break;
+
+      case TimePointFieldHelper.GRANULARITY_SECOND:
+        this.showDateButton = true
+        this.dialValues = [
+          this.current.format('A') + " " + this.current.format('HH'),
+          this.current.format('mm'),
+          this.current.format('ss'),
+        ]
+        break;
+    }
+  }
 }
--- client/app/research/omnitrack/field-inputs/timespan/timespan-field-mobile-input/timespan-field-mobile-input.component.ts
+++ client/app/research/omnitrack/field-inputs/timespan/timespan-field-mobile-input/timespan-field-mobile-input.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit, AfterContentChecked } from '@angular/core';
+import * as moment from 'moment';
+import { FieldMobileInputComponentBase } from '../../field-mobile-input-base.component';
+import { TimeSpanFieldHelper } from '../../../../../../../omnitrack/core/fields/time-span.field.helper';
 
 @Component({
   selector: 'app-timespan-field-mobile-input',
   templateUrl: './timespan-field-mobile-input.component.html',
   styleUrls: ['./timespan-field-mobile-input.component.scss']
 })
-export class TimeSpanFieldMobileInputComponent implements OnInit {
+export class TimeSpanFieldMobileInputComponent extends FieldMobileInputComponentBase<TimeSpanFieldHelper> implements OnInit, AfterContentChecked {
 
-  constructor() { }
+  current = moment(Date.now())
+  dateButtonFormat = 'LL'
+
+  footerButtonInfos = ["", "", ""]
+
+  constructor() {
+    super()
+  }
 
   ngOnInit() {
+  }
+
+  ngAfterContentChecked() {
+    switch (this.getFieldHelper().getParsedPropertyValue(this.field, TimeSpanFieldHelper.PROPERTY_GRANULARITY)) {
+      case TimeSpanFieldHelper.GRANULARITY_DAY:
+        this.dateButtonFormat = 'MMM D, ddd, YYYY'
+        this.footerButtonInfos = ["+ 1 day", "+ 1 week", "To now"]
+        break;
+      case TimeSpanFieldHelper.GRANULARITY_MINUTE:
+        this.dateButtonFormat = 'hh:mm A, MMM d, YYYY'
+        this.footerButtonInfos = ["+ 30 mins", "+ 1 hour", "To now"]
+        break;
+    }
   }
 
 }
