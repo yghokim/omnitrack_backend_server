8e9e917441fda105f93f259932bc86d5efe54cef
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2019-07-10 11:13:48 +0900

    added location and audio field input previews

--- client/app/research/omnitrack/field-inputs/image/image-field-mobile-input/image-field-mobile-input.component.ts
+++ client/app/research/omnitrack/field-inputs/image/image-field-mobile-input/image-field-mobile-input.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FieldMobileInputComponentBase } from '../../field-mobile-input-base.component';
+import { ImageFieldHelper } from '../../../../../../../omnitrack/core/fields/image.field.helper';
 
 @Component({
   selector: 'app-image-field-mobile-input',
   templateUrl: './image-field-mobile-input.component.html',
   styleUrls: ['./image-field-mobile-input.component.scss']
 })
-export class ImageFieldMobileInputComponent implements OnInit {
+export class ImageFieldMobileInputComponent extends FieldMobileInputComponentBase<ImageFieldHelper> implements OnInit {
 
-  constructor() { }
+  constructor() {
+    super()
+   }
 
   ngOnInit() {
   }
--- client/app/research/omnitrack/field-inputs/location/location-field-mobile-input/location-field-mobile-input.component.ts
+++ client/app/research/omnitrack/field-inputs/location/location-field-mobile-input/location-field-mobile-input.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FieldMobileInputComponentBase } from '../../field-mobile-input-base.component';
+import { LocationFieldHelper } from 'omnitrack/core/fields/location.field.helper';
 
 @Component({
   selector: 'app-location-field-mobile-input',
   templateUrl: './location-field-mobile-input.component.html',
   styleUrls: ['./location-field-mobile-input.component.scss']
 })
-export class LocationFieldMobileInputComponent implements OnInit {
+export class LocationFieldMobileInputComponent extends FieldMobileInputComponentBase<LocationFieldHelper> implements OnInit {
 
-  constructor() { }
+  constructor() { super() }
 
   ngOnInit() {
   }
