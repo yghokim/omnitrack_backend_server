4db2dae35b4b51836414a51263292f7fb7a77ae0
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2019-07-11 20:02:35 +0900

    implemented drag panning

--- client/app/material-design.module.ts
+++ client/app/material-design.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
@@ -7,7 +6,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatIconRegistry, MatIconModule } from '@angular/material/icon';
+import { MatIconRegistry } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
@@ -24,10 +23,10 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatListModule } from '@angular/material/list';
-import {MatStepperModule} from '@angular/material/stepper';
+import { MatStepperModule } from '@angular/material/stepper';
 import { MatNativeDateModule } from '@angular/material';
-import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
-import {MatRippleModule} from '@angular/material/core'; 
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatRippleModule } from '@angular/material/core';
 
 @NgModule({
   exports: [
--- client/app/research/omnitrack/tracking-plan-detail/tracker-preview-panel/tracker-preview-panel.component.ts
+++ client/app/research/omnitrack/tracking-plan-detail/tracker-preview-panel/tracker-preview-panel.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ElementRef, ViewChild, HostListener } from '@angular/core';
 import { TrackingPlan } from '../../../../../../omnitrack/core/tracking-plan';
 import { TrackingPlanService } from '../../tracking-plan.service';
 import { Subscription } from 'rxjs';
 import { ResizedEvent } from 'angular-resize-event';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tracker-preview-panel',
@@ -27,7 +28,26 @@ export class TrackerPreviewPanelComponent implements OnInit, OnDestroy {
   public dY: number = 0
   public scale: number = 0.5
 
-  constructor(private planService: TrackingPlanService) {
+  readonly maxScale = 3
+
+  get scalePercent(): number{
+    return (Math.round(this.scale * 10000)/100)
+  }
+
+  set scalePercent(percent: number){
+    this.scale = this.clamp(Math.round(percent*100)/10000, this.minimumZoomLevel(), this.maxScale)
+  }
+
+  viewPortMouseDownX: number = null
+  viewPortMouseDownY: number = null
+  viewPortMouseDownScrollLeft: number = null
+  viewPortMouseDownScrollTop: number = null
+
+
+  @ViewChild("viewPort")
+  viewPortRef: ElementRef
+
+  constructor(private planService: TrackingPlanService, private sanitizer: DomSanitizer) {
     this.plan = planService.currentPlan
   }
 
@@ -39,9 +59,7 @@ export class TrackerPreviewPanelComponent implements OnInit, OnDestroy {
   }
 
   onContentResized(event: ResizedEvent) {
-    console.log("width: ", event.oldWidth, " to ", event.newWidth)
-    console.log("height: ", event.oldHeight, " to ", event.newHeight)
-    
+
     this.contentWidth = event.newWidth
     this.contentHeight = event.newHeight
     this.scale = Math.max(this.minimumZoomLevel(), this.scale)
@@ -63,24 +81,31 @@ export class TrackerPreviewPanelComponent implements OnInit, OnDestroy {
 
   centerBiasX(): string {
     const realContentWidth = this.contentWidth * this.scale
-    if (realContentWidth < this.viewPortWidth - 2*this.horizontalPaddingSize) {
-      return ((this.viewPortWidth - (realContentWidth + 2*this.horizontalPaddingSize*this.scale)) * .5) + "px"
-    } else return null
+    if (realContentWidth < this.viewPortWidth - 2 * this.horizontalPaddingSize) {
+      return ((this.viewPortWidth - (realContentWidth + 2 * this.horizontalPaddingSize * this.scale)) * .5) + "px"
+    } else return "0px"
   }
 
 
   centerBiasY(): string {
     const realContentHeight = this.contentHeight * this.scale
-    if (realContentHeight < this.viewPortHeight - 2*this.verticalPaddingSize) {
-      return ((this.viewPortHeight - (realContentHeight + 2*this.verticalPaddingSize*this.scale)) * .5) + "px"
-    } else return null
+    if (realContentHeight < this.viewPortHeight - 2 * this.verticalPaddingSize) {
+      return ((this.viewPortHeight - (realContentHeight + 2 * this.verticalPaddingSize * this.scale)) * .5) + "px"
+    } else return "0px"
+  }
+
+  makeTransform(): SafeStyle {
+    const bX = this.centerBiasX()
+    const bY = this.centerBiasY()
+    const translate = (bX != null && bY != null) ? ("translate(" + bX + "," + bY + ")") : ""
+    return this.sanitizer.bypassSecurityTrustStyle(translate + "scale(" + this.scale + ")")
   }
 
   minimumZoomLevel(): number {
     if (this.contentWidth > 0 && this.contentHeight > 0) {
       const horizontalLevel = Math.max((this.viewPortWidth - 20), 100) / (this.contentWidth + 2 * this.horizontalPaddingSize)
 
-      const verticalLevel = Math.max((this.viewPortHeight - 20), 100)/ (this.contentHeight + 2 * this.verticalPaddingSize)
+      const verticalLevel = Math.max((this.viewPortHeight - 20), 100) / (this.contentHeight + 2 * this.verticalPaddingSize)
       return Math.min(horizontalLevel, verticalLevel)
     } else return 1
   }
@@ -91,11 +116,58 @@ export class TrackerPreviewPanelComponent implements OnInit, OnDestroy {
   }
 
   zoomIn() {
-    this.scale += 0.1;
+    this.scale = this.clamp(this.scale + 0.1, this.minimumZoomLevel(), this.maxScale);
   }
 
-  fitView(){
+  fitView() {
     console.log(this.minimumZoomLevel())
     this.scale = this.minimumZoomLevel()
   }
+
+
+
+  onMouseDownInViewPort(event: MouseEvent) {
+    console.log("startDrag")
+    this.viewPortMouseDownX = event.clientX
+    this.viewPortMouseDownY = event.clientY
+
+    this.viewPortMouseDownScrollLeft = this.viewPortRef.nativeElement.scrollLeft
+    this.viewPortMouseDownScrollTop = this.viewPortRef.nativeElement.scrollTop
+    event.stopPropagation()
+  }
+
+  @HostListener('mouseup', ['$event'])
+  onMouseUp(event: MouseEvent) {
+    this.viewPortMouseDownX = null
+    this.viewPortMouseDownY = null
+
+    this.viewPortMouseDownScrollLeft = null
+    this.viewPortMouseDownScrollTop = null
+  }
+
+  onMouseMoveInViewPort(event: MouseEvent) {
+    if (this.viewPortMouseDownX != null) {
+      const deltaX = event.clientX - this.viewPortMouseDownX
+      const deltaY = event.clientY - this.viewPortMouseDownY
+
+      const newScrollLeft = this.clamp(this.viewPortMouseDownScrollLeft - deltaX, 0, this.viewPortRef.nativeElement.scrollLeftMax)
+      const newScrollTop = this.clamp(this.viewPortMouseDownScrollTop - deltaY, 0, this.viewPortRef.nativeElement.scrollTopMax)
+
+      this.viewPortRef.nativeElement.scrollLeft = newScrollLeft
+      this.viewPortRef.nativeElement.scrollTop = newScrollTop
+
+      event.stopPropagation()
+    }
+  }
+
+  clamp(value, min, max): number {
+    return Math.min(max, Math.max(value, min))
+  }
+
+  //scroll
+  //scrollLeft: 212
+  //scrollLeftMax: 212
+  //scrollTop: 200
+  //scrollTopMax: 1920
+
 }
