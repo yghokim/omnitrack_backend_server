75eec47c50774a0c06d2bc192fa9eb51b84f4538
Young-Ho Kim <yghokim@umd.edu>
2020-07-14 12:13:46 -0400

    Support downloading the audio file in cell.

--- client/app/experiment-data/audio-cell/audio-cell.component.ts
+++ client/app/experiment-data/audio-cell/audio-cell.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit, Input, Inject, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, Inject, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { ITrackerDbEntity, IFieldDbEntity } from '../../../../omnitrack/core/db-entity-types';
 import { ResearchApiService } from '../../services/research-api.service';
 import { SingletonAudioPlayerServiceService } from '../../services/singleton-audio-player-service.service';
-import { Subscription ,  BehaviorSubject} from 'rxjs';
+import { Subscription, BehaviorSubject } from 'rxjs';
 import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 import { MatIconRegistry } from '@angular/material';
 import { Observable, timer } from 'rxjs';
 import { Pipe, PipeTransform } from '@angular/core';
+import * as FileSaver from 'file-saver';
+import { filter } from 'rxjs/operators';
 
 
 @Component({
@@ -14,7 +16,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   templateUrl: './audio-cell.component.html',
   styleUrls: ['./audio-cell.component.scss']
 })
-export class AudioCellComponent implements OnInit {
+export class AudioCellComponent implements OnInit, OnDestroy {
 
   private _internalSubscriptions = new Subscription();
   private timeSubscription: Subscription;
@@ -26,29 +28,32 @@ export class AudioCellComponent implements OnInit {
   private timer: Observable<number>;
   public isPlaying: boolean = false;
 
-  constructor(private api: ResearchApiService, private sanitizer: DomSanitizer, private singletonService: SingletonAudioPlayerServiceService) {  }
+  private audioBlob = new BehaviorSubject<{ blob: Blob, uniqueFileName: string }>(null);
+
+  constructor(private api: ResearchApiService, private sanitizer: DomSanitizer, private singletonService: SingletonAudioPlayerServiceService) { }
 
   ngOnInit() {
-    this.audioElement = this.audioElement.nativeElement;  
+    this.audioElement = this.audioElement.nativeElement;
   }
 
-  startTimer(){
-    this.timer = timer(0,1000);
+  startTimer() {
+    this.timer = timer(0, 1000);
     this.timeSubscription = new Subscription();
-    this.timeSubscription = this.timer.subscribe(t=> {
-      this.currentTime.next(this.audioElement.currentTime); 
-      if(this.audioElement.currentTime === this.audioElement.duration){
+    this.timeSubscription = this.timer.subscribe(t => {
+      this.currentTime.next(this.audioElement.currentTime);
+      if (this.audioElement.currentTime === this.audioElement.duration) {
         this.stop();
-      } 
+      }
     });
     this._internalSubscriptions.add(this.timeSubscription);
   }
 
   @Input("mediaInfo")
-  set _mediaInfo(info: {trackerId: string, fieldLocalId: string, itemId: string }){
+  set _mediaInfo(info: { trackerId: string, fieldLocalId: string, itemId: string }) {
     console.log("load audio data")
     this._internalSubscriptions.add(
       this.api.getMedia(info.trackerId, info.fieldLocalId, info.itemId, "").subscribe(response => {
+        this.audioBlob.next({ blob: response, uniqueFileName: info.trackerId + "_" + info.itemId + "_" + info.fieldLocalId + ".wav" })
         this.createAudio(response);
       }, err => {
       })
@@ -58,35 +63,35 @@ export class AudioCellComponent implements OnInit {
   createAudio(audio: Blob): void {
     let reader = new FileReader();
     reader.addEventListener("load", () => {
-       this.audioSource = reader.result;
+      this.audioSource = reader.result;
     }, false);
 
     if (audio) {
-       reader.readAsDataURL(audio);
+      reader.readAsDataURL(audio);
     }
   }
 
-  playAudio(){
-   
-    this.audioDuration = this.audioElement.duration; 
-    let play = this.singletonService.playAudio( this.audioElement, this);
-    if(play === false){
+  playAudio() {
+
+    this.audioDuration = this.audioElement.duration;
+    let play = this.singletonService.playAudio(this.audioElement, this);
+    if (play === false) {
       this.stop();
     }
-    else{
+    else {
       this.play();
     }
   }
 
-  stop(){
+  stop() {
     this.audioElement.pause();
     this.audioElement.currentTime = 0;
     this.currentTime.next(0);
     this.isPlaying = false;
     this.timeSubscription.unsubscribe();
   }
-  
-  play(){
+
+  play() {
     this.audioElement.load();
     this.audioElement.play();
     this.startTimer();
@@ -97,6 +102,12 @@ export class AudioCellComponent implements OnInit {
     this._internalSubscriptions.unsubscribe();
   }
 
+  downloadAudio() {
+    this._internalSubscriptions.add(this.audioBlob.pipe(filter(b => b != null)).subscribe(info => {
+      FileSaver.saveAs(info.blob, info.uniqueFileName)
+    })
+    )
+  }
 }
 
 @Pipe({
@@ -104,14 +115,14 @@ export class AudioCellComponent implements OnInit {
 })
 export class MinuteSecondsPipe implements PipeTransform {
 
-    transform(value: number): string {
-       const minutes: number = Math.floor(value / 60);
-       const seconds: number =  Math.round(value - minutes * 60);
-       if(seconds < 10){
-        return minutes + ':0' + seconds;
-       }
-       else return minutes + ':' + seconds;
-        
+  transform(value: number): string {
+    const minutes: number = Math.floor(value / 60);
+    const seconds: number = Math.round(value - minutes * 60);
+    if (seconds < 10) {
+      return minutes + ':0' + seconds;
     }
+    else return minutes + ':' + seconds;
+
+  }
 
 }
